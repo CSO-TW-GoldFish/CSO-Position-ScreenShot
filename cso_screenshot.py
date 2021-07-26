f95cf4acc7588bdaf657422d398a12621309b3ab
CSO-TW-GoldFish <73050517+CSO-TW-GoldFish@users.noreply.github.com>
2021-07-26 12:52:45 +0800

    Upload

---
+++ cso_screenshot.py
@@ -0,0 +1,53 @@
+from win32gui import FindWindow, GetWindowRect, GetWindowDC, DeleteObject, ReleaseDC
+from win32ui import CreateDCFromHandle, CreateBitmap
+from win32con import SRCCOPY
+from PIL.Image import frombuffer
+import time
+import cv2 as cv
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+# source https://www.programmersought.com/article/37942783573/
+def screenshot(name):
+    hWnd = FindWindow(None, "Counter-Strike Online")
+    if not hWnd: return None
+    left, top, right, bot = GetWindowRect(hWnd)
+    width = right - left
+    height = bot - top
+    x = int(width / 2) + 10
+    y = int(height / 2) + 15
+
+    hWndDC = GetWindowDC(hWnd)
+    mfcDC = CreateDCFromHandle(hWndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+    saveBitMap = CreateBitmap()
+
+    saveBitMap.CreateCompatibleBitmap(mfcDC,110,16)
+    saveDC.SelectObject(saveBitMap)
+    saveDC.BitBlt((-x,-y), (width,height), mfcDC, (0, 0), SRCCOPY)
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im_PIL = frombuffer('RGB',(bmpinfo['bmWidth'],bmpinfo['bmHeight']),bmpstr,'raw','BGRX',0,1)
+
+    DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    ReleaseDC(hWnd,hWndDC)
+
+    im_PIL.save(name)
+    return True
+
+
+def convertGrayImage(name, newName):
+    # 載入圖片
+    img = cv.imread(name)
+    # 改變圖片尺吋
+    img = cv.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
+    # 灰階
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, thresh = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
+    cv.imwrite(newName, thresh)
---
+++ main.py
@@ -0,0 +1,62 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPlainTextEdit, QPushButton, QLabel
+
+from PIL import Image
+from cso_screenshot import *
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.tooltip = QLabel("請使用視窗化哦! by 崩潰金魚燒 <a href='https://www.youtube.com/'>Github</a>")
+        self.tooltip.setOpenExternalLinks(True)
+        self.tooltip.setStyleSheet("color: red;")
+        self.setWindowTitle("CSO Studio 截圖自動轉換座標")
+        self.convertText = QPlainTextEdit()
+        self.convertText.setStyleSheet("font-size: 16px;font-family: Segoe UI")
+        self.convertBtn = QPushButton("截圖並轉換")
+        self.convertBtn.clicked.connect(self.ScreenShotAndConvertPosition)
+        self.convertBtn.setMinimumHeight(60)
+        self.clearTextBtn = QPushButton("清除文字")
+        self.clearTextBtn.clicked.connect(lambda: self.convertText.clear())
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.tooltip)
+        self.mainLayout.addWidget(self.convertText)
+        self.mainLayout.addWidget(self.convertBtn)
+        self.mainLayout.addWidget(self.clearTextBtn)
+        self.setLayout(self.mainLayout)
+    
+    def ScreenShotAndConvertPosition(self):
+        error = ""
+        fileName = "position.png"
+        newName = "newPosition.png"
+
+        hasImage = screenshot(fileName)
+        if not hasImage:
+            error = "未偵測到遊戲!"
+        else:
+            convertGrayImage(fileName, newName)
+
+            img = Image.open(newName)
+            text = pytesseract.image_to_string(img, lang='eng',config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789-,[]')
+
+            tmp_pos = text.strip().split(",")
+
+            try:
+                x = tmp_pos[0]
+                y = tmp_pos[1]
+                z = tmp_pos[2]
+                error = "{" + f"x = {x}, y = {y}, z = {z}" + "}"
+            except IndexError:
+                error = "無法分析!"
+
+        self.convertText.appendPlainText(error)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
